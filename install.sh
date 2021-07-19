8b0b245b72aeabf048fa5b09f38ec19b85f357d6
313 <36674091+Node313@users.noreply.github.com>
2021-07-20 01:04:52 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,49 @@
+!#/bin/bash
+
+termux-wake-unlock
+apt-get update
+apt-get install git -y
+apt-get install cmake -y
+apt update
+apt upgrade -y
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake -DWITH_HWLOC=OFF ..
+make
+
+#Ask for pool
+PS3='Where do you want to mine? '
+pool=("Swepool.org:3333" "Norpool.org:3333" "Pool.kryptokrona.se:3333" "Pool.gamersnest.org:3333")
+select opt in "${pool[@]}"
+do
+    case $opt in
+        "Swepool.org:3333")
+            echo "Swepool it is!"
+            break
+            ;;
+        "Norpool.org:3333")
+            echo "Norpool it is!"
+            break
+            ;;
+        "Pool.kryptokrona.se:3333")
+            echo "Kryptokrona it is!"
+            break
+            ;;
+        "Pool.gamersnest.org:3333")
+            break
+            ;;
+        *) echo "Gamersnest it is!";;
+    esac
+done
+
+#Ask for address
+
+echo Your XKR-Address?
+
+read address
+
+#Start xmrig
+
+./xmrig -a cn-pico -o $pool -u $address -p x -t 8
